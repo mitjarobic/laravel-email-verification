c99052cfcd9c8ab4463a2ac359d2f117cc029310
Mitja <mitja.robic@gmail.com>
2017-03-17 22:29:11 +0100

    disabled loading migrations in provider

--- src/Providers/EmailVerificationServiceProvider.php
+++ src/Providers/EmailVerificationServiceProvider.php
@@ -29,7 +29,7 @@ class EmailVerificationServiceProvider extends ServiceProvider
         /*
          * Migrations
          */
-        $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
+        //$this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
         $this->publishes([
             __DIR__ . '/../../database/migrations' => database_path('migrations')
         ], 'migrations');
