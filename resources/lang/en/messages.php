80640af9c7d7dbded108d791061f98b82618286a
Mitja <mitja.robic@gmail.com>
2017-04-06 22:12:49 +0200

    add notification messages

--- resources/lang/en/messages.php
+++ resources/lang/en/messages.php
@@ -10,5 +10,11 @@ return [
     'sent' => 'We have e-mailed your account activation link!',
     'token' => 'This email verification token is invalid.',
     'user' => "We can't find a user with that e-mail address.",
+    
+    'notification' =>[
+        'thank_you' => 'Thank you for verifying!',
+        'almost_done' => 'You\'re almost done! Please click here to complete your registration:',
+        'verify' => 'Verify',
+    ]
 
 ];
